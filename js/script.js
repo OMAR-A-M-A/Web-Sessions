6fab33be6fdb29116a9221d8a766a19d40d448e9
OMAR-A-M-A <oa5252842@gmail.com>
2025-10-07 18:37:36 +0300

    css styling for cards

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const sessions = [
     img: "img/html5S1.png",
     url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
     category: "html",
+    icon: "fa-solid fa-code",
   },
   {
     title: "HTML Basics",
@@ -19,6 +20,77 @@ const sessions = [
     img: "img/html5S1.png",
     url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
     category: "html",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "css",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "css",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "css",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "js",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "js",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "js",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "js",
+  },
+
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "bootstrap",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "bootstrap",
+  },
+  {
+    title: "HTML Basics",
+    topics: "Introduction to tags, elements, attributes, and structure.",
+    img: "img/html5S1.png",
+    url: "https://appflowy.com/90507d36-0c51-4ff3-8366-1318cada01ae/HTML5-Session-1-copy",
+    category: "bootstrap",
   },
 ];
 
@@ -30,9 +102,11 @@ function displaySessions() {
     card.classList.add("session-card");
 
     card.innerHTML = `
+          <div class="imgs">
           <img src="${session.img}" alt="${session.title}">
+          </div>
           <div class="session-content">
-            <h3>${session.title}</h3>
+            <h3><i class="${session.icon}"></i> ${session.title}</h3>
             <p>${session.topics}</p>
           </div>
           <button>View Session</button>
